2b76cd01ac0eae4e074fda79fdecf5010baa2fea
HanouneCamara <Alicmr13@gmail.com>
2023-05-28 19:22:47 +0000

    test

--- lib/src/features/authentification/screens/annonces/publicationAnnoncePage.dart
+++ lib/src/features/authentification/screens/annonces/publicationAnnoncePage.dart
@@ -1,71 +1,30 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
-import 'package:multi_image_picker/multi_image_picker.dart';
-import 'package:image_picker/image_picker.dart';
-
 
 class PublicationAnnoncePage extends StatefulWidget {
-  const PublicationAnnoncePage({Key? key}) : super(key: key);
+  const PublicationAnnoncePage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _PublicationAnnoncePageState createState() => _PublicationAnnoncePageState();
 }
 
 class _PublicationAnnoncePageState extends State<PublicationAnnoncePage> {
-  String? _selectedStatut;
+
+   String? _selectedStatut;
   final List<String> _statuts = ['En vente', 'En location', 'Les deux'];
 
-  String? _selectedCarburant;
+  String? _selectedcarburants;
   final List<String> _carburants = ['Essence', 'Gazoil'];
 
-  String? _selectedTransmission;
-  final List<String> _transmissions = ['Manuelle', 'Automatique'];
+  String? _selectedtransmission;
+  final List<String> _transmission = ['Manuelle', 'Automatique'];
 
-
-  List<Asset> _selectedImages = <Asset>[];
-
-  void _pickImages() async {
-    List<Asset> resultList = <Asset>[];
-    String? error;
-
-    try {
-      resultList = await MultiImagePicker.pickImages(
-        maxImages: 5, // Limite maximale de 5 images
-        enableCamera: true, // Activer l'option d'appareil photo
-        materialOptions: MaterialOptions(
-          actionBarTitle: 'Sélectionnez des images', // Titre de l'action bar
-        ),
-      );
-    } catch (e) {
-      error = e.toString();
-    }
-
-    if (!mounted) return;
-
-    if (error == null && resultList.isNotEmpty) {
-      setState(() {
-        _selectedImages = resultList;
-      });
-    } else if (error != null) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Erreur lors de la sélection des images'),
-            content: Text(error!),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  }
+  
 
   final _formKey = GlobalKey<FormState>();
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,181 +32,226 @@ class _PublicationAnnoncePageState extends State<PublicationAnnoncePage> {
         title: const Text('Publication d\'annonce'),
       ),
       body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(8.0),
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Marque',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          key: _formKey,
+          child: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Marque',
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(20.0)))),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a Marque';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Veuillez entrer une marque';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Modèle',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Modèle',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an modele';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Veuillez entrer un modèle';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Année',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Année',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an Année';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Veuillez entrer une année';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Kilométrage',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Kilométrage',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an Kilométrage';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Veuillez entrer un kilométrage';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8.0),
-            DropdownButtonFormField<String>(
-              value: _selectedCarburant,
-              items: _carburants.map((String carburant) {
-                return DropdownMenuItem<String>(
-                  value: carburant,
-                  child: Text(carburant),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedCarburant = newValue;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Type de carburant',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: DropdownButtonFormField<String>(
+                  
+                  value: _selectedcarburants,
+                  items: _carburants.map((String carburants) {
+                    return DropdownMenuItem<String>(
+                      value: carburants,
+                      child: Text(carburants)
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedcarburants = newValue;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Type de carburant',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            DropdownButtonFormField<String>(
-              value: _selectedTransmission,
-              items: _transmissions.map((String transmission) {
-                return DropdownMenuItem<String>(
-                  value: transmission,
-                  child: Text(transmission),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedTransmission = newValue;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Type de transmission',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: DropdownButtonFormField<String>(
+                  
+                  value: _selectedtransmission,
+                  items: _transmission.map((String transmission) {
+                    return DropdownMenuItem<String>(
+                      value: transmission,
+                      child: Text(transmission)
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedtransmission = newValue;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Type de transmission',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            DropdownButtonFormField<String>(
-              value: _selectedStatut,
-              items: _statuts.map((String statut) {
-                return DropdownMenuItem<String>(
-                  value: statut,
-                  child: Text(statut),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedStatut = newValue;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: 'Statut de la voiture',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: DropdownButtonFormField<String>(
+                  
+                  value: _selectedStatut,
+                  items: _statuts.map((String statut) {
+                    return DropdownMenuItem<String>(
+                      value: statut,
+                      child: Text(statut)
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedStatut = newValue;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Statut de la voiture',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Prix',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Prix',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)
+                      )
+                    )
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter an Prix';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Veuillez entrer un prix';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8.0),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Description',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 16.0, right: 16.0, bottom: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Description',
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(20.0)))),
+                  keyboardType: TextInputType.multiline,
+                  minLines: 3,
+                  maxLines: 5,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a description';
+                    }
+                    return null;
+                  },
                 ),
               ),
-              keyboardType: TextInputType.multiline,
-              minLines: 3,
-              maxLines: 5,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Veuillez entrer une description';
-                }
-                return null;
-              },
-            ),
 
               const SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: _pickImages,
-              child: const Text('Importer des images'),
-              
-            ),
-
-            const SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Enregistrer'),
-            ),
-          ],
-        ),
-      ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: const Text('Save'),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
